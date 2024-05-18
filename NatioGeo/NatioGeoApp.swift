e0d7eb44a188094002f9f2936ab94555aeb038a0
mohamed930 <mohamed@gmail.com>
2024-05-18 15:37:40 +0300

    Initial Commit

---
+++ NatioGeo/NatioGeoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NatioGeoApp.swift
+//  NatioGeo
+//
+//  Created by Mohamed Ali on 18/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct NatioGeoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
